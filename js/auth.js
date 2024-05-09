af5f1f3b504b65fb8c08c4be06a2217dda3b4bec
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-05-09 18:45:47 -0300

    feat: :sparkles: Redefinir senha
    Função que permite o usuário redefinir a senha dele

--- js/auth.js
+++ js/auth.js
@@ -20,7 +20,7 @@ authForm.onsubmit = function (e) {
     }
 }
 
-// função que centraliza e trata autenticação
+// Função que centraliza e trata autenticação
 firebase.auth().onAuthStateChanged(function (user) {
     hideItem(loading)
     if (user) {
@@ -30,7 +30,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 })
 
-// função que permite ao usuário sair da conta dele
+// Função que permite ao usuário sair da conta dele
 function signOut() {
     firebase.auth().signOut().catch(function (error) {
         console.log('falha ao sair da conta');
@@ -38,7 +38,7 @@ function signOut() {
     })
 }
 
-// função que permite ao usuario  fazer a verificação do e-mail
+// Função que permite ao usuario  fazer a verificação do e-mail
 function sendEmailVerification() {
     showItem(loading)
     var user = firebase.auth().currentUser
@@ -48,4 +48,22 @@ function sendEmailVerification() {
         alert('Houve um erro ao enviar o e-mail de verificação')
         console.log({ first })
     }).finally(function () { hideItem(loading) })
+}
+
+// Função que permite o usuário redefinir a senha dele
+function sendPasswordResetEmail() {
+    var email = prompt('Redefinir senha! Informe o seu endereço de e-mail.', authForm.email.value);
+    if (email) {
+        showItem(loading);
+        firebase.auth().sendPasswordResetEmail(email, actionCodeSettings).then(function () {
+            alert('E-mail de redefinição de foi enviado para' + email + '.') ;
+        }).catch(function (err) {
+            alert('Houve um erro ao enviar e-mail de redefinição de senha!')
+            console.log(err) ;
+        }).finally(function () {
+            hideItem(loading)
+        })
+    } else {
+        alert('É preciso preencher o campo de e-mail para redefinir senha.')
+    }
 }
